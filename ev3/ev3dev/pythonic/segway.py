566baf14eabd7d5adc3a570fc0ffeece23bac35a
Laurens Valk <laurensvalk@gmail.com>
2018-04-12 21:00:19 +0200

    angle high pass filter

--- ev3/ev3dev/pythonic/segway.py
+++ ev3/ev3dev/pythonic/segway.py
@@ -21,15 +21,19 @@ battery_scaling = nominal / battery.voltage
 
 # Configure and calibrate LEGO Gyro
 gyro = Gyro('in2', read_rate=True, read_angle=False, calibrate=True)
-initial_bias = 0
 
-# Configure filters
+# Configure highpass filters
 looptime = 0.02
-rate_highpass = Highpass(looptime, initial_bias, 0.02)
-differentiator = Differentiator(looptime, 5)
+rate_highpass = Highpass(looptime, 0, 0.02)
+angle_highpass = Highpass(looptime, 0, 0.02)
+
+# Angle rate and double integrator
 rate_integrator = Integrator(looptime)
 angle_integrator = Integrator(looptime)
+
+# Position differentiator and integrator
 distance_integrator = Integrator(looptime)
+distance_differentiator = Differentiator(looptime, 5)
 
 # Parameters
 deg2rad = 180/3.14
@@ -41,7 +45,7 @@ busytime = 0.005
 gain_rate = 1
 gain_angle = 70
 gain_angle_sum = 2
-gain_speed = 2.7+0.7+0.7
+gain_speed = 4.1
 gain_distance = 14
 gain_distance_sum = 2
 
@@ -52,14 +56,14 @@ while True:
 
     # Body angle
     rate = rate_highpass.filter(gyro.rate)
-    angle = rate_integrator.integral(rate)
+    angle = angle_highpass.filter(rate_integrator.integral(rate))
     angle_sum = angle_integrator.integral(angle)
 
     # Wheel translation
     average_motor_degrees = (right.position + left.position)/2
     distance = average_motor_degrees * cm_per_degree
     distance_sum = distance_integrator.integral(distance)
-    speed = differentiator.derivative(distance)
+    speed = distance_differentiator.derivative(distance)
 
     # Control signal
     duty = (gain_rate*rate +
